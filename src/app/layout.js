bf3d716e320b99cf762fa2e66283030e5ce1492f
Marufa Akter <marufaakter1947@gmail.com>
2025-11-23 11:33:32 +0600

    Navbar complete

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,8 @@
+
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/Components/Navbar";
+import Providers from "@/Components/Providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,7 +25,12 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+       <Providers>
+<Navbar></Navbar>
         {children}
+       </Providers>
+         
+       
       </body>
     </html>
   );
